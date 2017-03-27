390f1705de6047f5eee5b8ce65a57b4136aaea50
David Dong <dongm2ez@163.com>
2017-03-27 18:06:23 +0800

    文件系统迭代器补充

--- FileSystemIterator.php
+++ FileSystemIterator.php
@@ -1,7 +1,27 @@
 <?php
 /**
+ * spl 文件迭代器示例
  * Created by PhpStorm.
- * User: dongyuxiang
+ * User: m2ez
  * Date: 2017/3/27
- * Time: 18:01
+ * Time: 17:09
  */
+$fSI = new FileSystemIterator('.');
+/** @var FileSystemIterator $fInfo */
+
+// 模拟 windows dir
+foreach ($fSI as $fInfo) {
+    printf("%8s%8s%8s %8s\n",
+        date("Y-m-d H:i:s", $fInfo->getMtime()),
+        $fInfo->isDir() ? '<DIR>' : '',
+        number_format($fInfo->getSize()),
+        $fInfo->getFileName()
+    );
+}
+
+// 模拟 linux ls
+foreach ($fSI as $fInfo) {
+    printf("%8s  ",
+        $fInfo->getFileName()
+    );
+}
