15e75334d46fd01e702aa4db6645dd6b71ea643f
David Dong <dongm2ez@163.com>
2017-03-27 19:06:51 +0800

    文件操作

--- splFileInfo.php
+++ splFileInfo.php
@@ -11,3 +11,13 @@ $file = new SplFileInfo('README.md');
 echo 'File is created at ' . date('Y-m-d H:i:s', $file->getCTime()) . PHP_EOL;
 echo 'File is modified at ' . date('Y-m-d H:i:s', $file->getMTime()) . PHP_EOL;
 echo 'File size is ' . $file->getSize() . ' bytes' . PHP_EOL;
+
+// 读取文件里面的内容
+$fileObj = $file->openFile('r');
+while($fileObj->valid()){
+    echo $fileObj->fgets(); // 读取文件里的一行数据
+}
+
+// 关闭打开的文件
+$fileObj = null;
+$file = null;
